a830303caca3652635dad90e77225c1144f6d6c6
Gedion47 <gedionalemu279@gmail.com>
2025-09-20 20:25:27 +0300

    updated

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -5,7 +5,7 @@ export interface ButtonProps {
   title: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
-  styles?: string;
+  styles: string;
   size?: "small" | "medium" | "large";
   shape?: "rounded-sm" | "rounded-md" | "rounded-full";
 }
